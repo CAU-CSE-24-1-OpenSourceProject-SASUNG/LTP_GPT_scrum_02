4e18ced659d780a4528b1aa6d8b8bbb1a079f30f
Tastypotato245 <rbt2775@cau.ac.kr>
2024-05-04 03:06:27 +0900

    add: user icon and name

--- app/main.py
+++ app/main.py
@@ -74,8 +74,14 @@ async def auth(request: Request):
 
 @app.get("/quiz", response_class=HTMLResponse)
 async def home(request: Request):
+    user = request.session.get('user')
+    if not user:
+        return RedirectResponse('/')
     problem = "어떤 아이가 아파트 10층에 살고 있으며, 맑은 날에는 엘리베이터에서 6층에서 내려서 10층까지 걸어 올라간다. 그러나 날씨가 좋지 않다면 10층에서 내려서 집으로 간다. 어떤 상황일까?"
-    return templates.TemplateResponse("index.html", {"request": request, "problem": problem})
+    return templates.TemplateResponse(
+            name = 'index.html',
+            context={'request': request, 'problem' : problem, 'user': user}
+            )
 
 @app.post("/chat")
 async def chat(request: Request):
