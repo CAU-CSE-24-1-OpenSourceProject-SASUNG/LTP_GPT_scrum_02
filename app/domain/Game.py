b7967b977189d54141cd0e008725ed835ff81b34
zzeo14 <wodh7223@cau.ac.kr>
2024-05-21 15:17:32 +0900

    update: moduling database tables

--- app/domain/Game.py
+++ app/domain/Game.py
@@ -3,17 +3,18 @@ from sqlalchemy.orm import relationship
 
 from app.db.session import Base
 
-
 class Game(Base):
     __tablename__ = 'games'
 
-    game_id = Column(Integer, primary_key=True, autoincrement=True)
-    riddle_id = Column(Integer, ForeignKey('riddles.riddle_id'))
+    game_id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    riddle_id = Column(String(36), ForeignKey('riddles.riddle_id'))
     title = Column(String(255))
     createdAt = Column(DateTime)
     updatedAt = Column(DateTime)
     is_first = Column(Boolean)
+    progress = Column(Integer)
     query_count = Column(Integer)
+    correct_time = Column(Time)
     play_time = Column(Time)
     query_length = Column(Integer)
     hit = Column(Boolean)
--- app/domain/User.py
+++ app/domain/User.py
@@ -2,11 +2,10 @@ from sqlalchemy import Column, Integer, String, relationship
 
 from app.db.session import Base
 
-
 class User(Base):
     __tablename__ = 'users'
 
-    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     gmail = Column(String(255), primary_key=True)
     name = Column(String(255))
     experience = Column(Integer)
